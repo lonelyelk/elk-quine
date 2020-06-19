27685958578ddf9eb43ab7ad629cc9863411b33f
Sergey Kruk <sergey.kruk@gmail.com>
2020-06-19 11:42:12 +0200

    Make sha quine shorter by removing empty lines

--- sha.quine.rb
+++ sha.quine.rb
@@ -1,20 +1,14 @@
 require 'digest'
-
 sha = Digest.const_get(ARGV[0].to_s.upcase) rescue Digest::SHA1
-
 arr = [
   "require 'digest'",
-  "",
   "sha = Digest.const_get(ARGV[0].to_s.upcase) rescue Digest::SHA1",
-  "",
   "arr = [",
   "]",
-  "",
   "puts sha.hexdigest(",
-  "  [arr[0,5], arr.map { |l| '  ' + l.inspect + ',' }, arr[5..-1], ''].flatten.join(10.chr),",
+  "  [arr[0,3], arr.map { |l| '  ' + l.inspect + ',' }, arr[3..-1], ''].flatten.join(10.chr),",
   ")",
 ]
-
 puts sha.hexdigest(
-  [arr[0,5], arr.map { |l| '  ' + l.inspect + ',' }, arr[5..-1], ''].flatten.join(10.chr),
+  [arr[0,3], arr.map { |l| '  ' + l.inspect + ',' }, arr[3..-1], ''].flatten.join(10.chr),
 )
