84760f95e2f07cff4ba74dd4208304c6a023a1d2
Sergey Kruk <sergey.kruk@gmail.com>
2020-06-27 18:13:07 +0200

    Use predefined dollar const

--- sha.quine.rb
+++ sha.quine.rb
@@ -1,5 +1,5 @@
 eval s = <<-'RUBY'
 require 'digest'
-sha = Digest.const_get(ARGV[0].to_s.upcase) rescue Digest::SHA1
+sha = Digest.const_get($*[0].upcase) rescue Digest::SHA1
 puts sha.hexdigest("eval s = <<-'RUBY'\n#{s}RUBY\n") + '  ' + $0
 RUBY
