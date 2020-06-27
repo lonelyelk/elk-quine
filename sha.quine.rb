e57442ea1738672ce4dfff8aae293b3b5bbc410e
Sergey Kruk <sergey.kruk@gmail.com>
2020-06-27 13:37:51 +0200

    Use heredoc to profit from syntax highlight

--- sha.quine.rb
+++ sha.quine.rb
@@ -1,5 +1,5 @@
-eval a = <<-'a'
+eval a = <<-'RUBY'
 require 'digest'
 sha = Digest.const_get(ARGV[0].to_s.upcase) rescue Digest::SHA1
-puts sha.hexdigest("eval a = <<-'a'\n%sa\n" % [a])
-a
+puts sha.hexdigest("eval a = <<-'%<r>s'\n%<a>s%<r>s\n" % { r: 'RUBY', a: a })
+RUBY
