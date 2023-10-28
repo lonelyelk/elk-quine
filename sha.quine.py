3361551f601939746e4409be624f869f716e1100
Sergey Kruk <sergey.kruk@gmail.com>
2023-10-28 14:59:43 +0200

    Improve readability in sha python quine

--- sha.quine.py
+++ sha.quine.py
@@ -1,6 +1,21 @@
 import hashlib
 import sys
 s = """
-print(getattr(hashlib, (sys.argv[1:] or ['sha1'])[0])(str.encode(f'import hashlib{chr(10)}import sys{chr(10)}s = {chr(34) * 3}{s}{chr(34) * 3}{chr(10)}eval(s){chr(10)}')).hexdigest())
+print(
+  getattr(
+    hashlib,
+    (sys.argv[1:] or ['sha1'])[0]
+  )(
+    str.encode(
+      chr(10).join([
+        'import hashlib',
+        'import sys',
+        f's = {chr(34) * 3}{s}{chr(34) * 3}',
+        'eval(s)',
+        ''
+      ])
+    )
+  ).hexdigest()
+)
 """
 eval(s)
